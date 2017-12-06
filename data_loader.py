1553db1c0c1dd3f28da12cec71b65fe2748b5938
钱龙 <long.qian@ele.me>
2017-12-06 16:55:51 +0800

    ~

--- data_loader.py
+++ data_loader.py
@@ -10,7 +10,7 @@ def load_data(data_dir, flatten=False):
     train_dir = os.path.join(data_dir, 'train')
     test_dir = os.path.join(data_dir, 'test')
 
-    meta_info = os.path.join(current_dir, 'meta.json')
+    meta_info = os.path.join(data_dir, 'meta.json')
     with open(meta_info, 'r') as f:
         meta = json.load(f)
 
@@ -25,10 +25,9 @@ def load_data(data_dir, flatten=False):
 
 class DataSet:
     """提供 next_batch 方法"""
-    def __init__(self, images, labels_1, labels_2):
+    def __init__(self, images, labels):
         self._images = images
-        self._labels_1 = labels_1
-        self._labels_2 = labels_2
+        self._labels = labels
         self._num_examples = images.shape[0]
         self.ptr = 0
 
@@ -37,12 +36,8 @@ class DataSet:
         return self._images
 
     @property
-    def labels_1(self):
-        return self._labels_1
-
-    @property
-    def labels_2(self):
-        return self._labels_2
+    def labels(self):
+        return self._labels
 
     def next_batch(self, size=100, shuffle=True):
         if self.ptr + size > self._num_examples:
@@ -53,28 +48,24 @@ class DataSet:
                 perm = np.arange(self._num_examples)
                 np.random.shuffle(perm)
                 self._images = self._images[perm]
-                self._labels_1 = self._labels_1[perm]
-                self._labels_2 = self._labels_2[perm]
+                self._labels = self._labels[perm]
 
         self.ptr += size
         return (
             self._images[self.ptr - size: self.ptr],
-            self._labels_1[self.ptr - size: self.ptr],
-            self._labels_2[self.ptr - size: self.ptr],
+            self._labels[self.ptr - size: self.ptr],
         )
 
 
 def _read_images_and_labels(dir_name, flatten, ext='.png', **meta):
     images = []
-    labels_1 = []
-    labels_2 = []
+    labels = []
     for fn in os.listdir(dir_name):
         if fn.endswith(ext):
             fd = os.path.join(dir_name, fn)
             images.append(_read_image(fd, flatten=flatten, **meta))
-            labels_1.append(_read_lable(fd, 0, **meta))
-            labels_2.append(_read_lable(fd, 1, **meta))
-    return np.array(images), np.array(labels_1), np.array(labels_2)
+            labels.append(_read_lable(fd, **meta))
+    return np.array(images), np.array(labels)
 
 
 def _read_image(filename, flatten, width, height, **extra_meta):
@@ -84,13 +75,15 @@ def _read_image(filename, flatten, width, height, **extra_meta):
         return data.reshape(width * height)
     return data
 
-def _read_lable(filename, index, label_choices, **extra_meta):
+def _read_lable(filename, label_choices, **extra_meta):
     basename = os.path.basename(filename)
-    data = np.zeros(len(label_choices))
-    c = basename.split('_')[0][index]
-    idx = label_choices.index(c)
-    data[idx] = 1
-    return data
+    data_list = []
+    for c in basename.split('_')[0]:
+        data = np.zeros(len(label_choices))
+        idx = label_choices.index(c)
+        data[idx] = 1
+        data_list.append(data)
+    return np.concatenate(data_list)
 
 if __name__ == '__main__':
     meta, train, test = load_data('images/char-2-groups-10')
--- softmax_demo.py
+++ softmax_demo.py
@@ -2,6 +2,7 @@
 import argparse
 import sys
 import tensorflow as tf
+import numpy as np
 
 import data_loader
 
@@ -13,56 +14,51 @@ def main(image_dir):
     meta, train_data, test_data = data_loader.load_data(image_dir, flatten=True)
     print 'train images: %s. test images: %s' % (train_data.images.shape[0], test_data.images.shape[0])
 
+    NUM_PER_IMAGE = meta['num_per_image']
     LABEL_SIZE = meta['label_size']
     IMAGE_SIZE = meta['width'] * meta['height']
     print 'label_size: %s, image_size: %s' % (LABEL_SIZE, IMAGE_SIZE)
 
     # variable in the graph for input data
     x = tf.placeholder(tf.float32, [None, IMAGE_SIZE])
-    y1_ = tf.placeholder(tf.float32, [None, LABEL_SIZE])
-    y2_ = tf.placeholder(tf.float32, [None, LABEL_SIZE])
+    y_ = [tf.placeholder(tf.float32, [None, LABEL_SIZE]) for _ in range(NUM_PER_IMAGE)]
+
+    test_mapping = {label: ys for label, ys in zip(y_, np.split(test_data.labels, NUM_PER_IMAGE, axis=1))}
+    test_mapping[x] = test_data.images
 
     # define the model
-    W1 = tf.Variable(tf.zeros([IMAGE_SIZE, LABEL_SIZE]))
-    b1 = tf.Variable(tf.zeros([LABEL_SIZE]))
-    y1 = tf.matmul(x, W1) + b1
-
-    W2 = tf.Variable(tf.zeros([IMAGE_SIZE, LABEL_SIZE]))
-    b2 = tf.Variable(tf.zeros([LABEL_SIZE]))
-    y2 = tf.matmul(x, W2) + b2
+    y = [tf.layers.dense(x, LABEL_SIZE) for _ in range(NUM_PER_IMAGE)]
 
     # Define loss and optimizer
-    diff_1 = tf.nn.softmax_cross_entropy_with_logits(labels=y1_, logits=y1)
-    diff_2 = tf.nn.softmax_cross_entropy_with_logits(labels=y2_, logits=y2)
-    cross_entropy = tf.reduce_mean(diff_1) + tf.reduce_mean(diff_2)
+    cross_entropy = 0
+    accuracy = 0
+    for label, logit in zip(y_, y):
+        diff = tf.nn.softmax_cross_entropy_with_logits(labels=label, logits=logit)
+        cross_entropy += tf.reduce_mean(diff)
+
+        predict = tf.argmax(logit, axis=1)
+        expect = tf.argmax(label, axis=1)
+        correct_prediction = tf.equal(predict, expect)
+        accuracy += tf.reduce_mean(tf.cast(correct_prediction, tf.float32)) 
     train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-
-    # forword prop
-    predict_1 = tf.argmax(y1, axis=1)
-    predict_2 = tf.argmax(y2, axis=1)
-    expect_1 = tf.argmax(y1_, axis=1)
-    expect_2 = tf.argmax(y2_, axis=1)
-
-    # evaluate accuracy
-    correct_prediction_1 = tf.equal(predict_1, expect_1)
-    correct_prediction_2 = tf.equal(predict_2, expect_2)
-    accuracy = (tf.reduce_mean(tf.cast(correct_prediction_1, tf.float32)) + 
-            tf.reduce_mean(tf.cast(correct_prediction_2, tf.float32))) / 2.0
+    accuracy /= NUM_PER_IMAGE
 
     with tf.Session() as sess:
         tf.global_variables_initializer().run()
 
         # Train
         for i in range(MAX_STEPS):
-            batch_xs, batch_ys_1, batch_ys_2 = train_data.next_batch(BATCH_SIZE)
-            sess.run(train_step, feed_dict={x: batch_xs, y1_: batch_ys_1, y2_: batch_ys_2})
+            batch_xs, batch_ys = train_data.next_batch(BATCH_SIZE)
+            mapping = {label: ys for label, ys in zip(y_, np.split(batch_ys, NUM_PER_IMAGE, axis=1))}
+            mapping[x] = batch_xs
+            sess.run(train_step, feed_dict=mapping)
 
             if i % 100 == 0:
                 # Test trained model
-                r = sess.run(accuracy, feed_dict={x: test_data.images, y1_: test_data.labels_1, y2_:test_data.labels_2})
+                r = sess.run(accuracy, feed_dict=test_mapping)
                 print 'step = %s, accuracy = %.2f%%' % (i, r * 100)
         # final check after looping
-        r_test = sess.run(accuracy, feed_dict={x: test_data.images, y1_: test_data.labels_1, y2_:test_data.labels_2})
+        r_test = sess.run(accuracy, feed_dict=test_mapping)
         print 'testing accuracy = %.2f%%' % (r_test * 100, )
 
 
